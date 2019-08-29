cfabcc7b5f4c688f3b2e1b3766951ebe99fb4b0a
Spyder #DerpGang <spyderzzz.cbc@gmail.com>
2019-08-29 19:30:51 +0530

    Update generate_session_file.py

--- generate_session_file.py
+++ generate_session_file.py
@@ -11,8 +11,8 @@ import os
 
 load_dotenv("config.env")
 
-API_KEY = os.environ.get("API_KEY", None)
-API_HASH = os.environ.get("API_HASH", None)
+API_KEY = os.environ.get("API_KEY", "930707")
+API_HASH = os.environ.get("API_HASH", "61db5228770fa752f871b4db48a9393f")
 
 bot = TelegramClient('userbot', API_KEY, API_HASH)
 bot.start()
