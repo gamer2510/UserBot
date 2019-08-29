5146f533a8b47e30ac2d9ff45531c7f4420be7e6
Spyder #DerpGang <spyderzzz.cbc@gmail.com>
2019-08-29 19:42:15 +0530

    Update generate_session_file.py

--- generate_session_file.py
+++ generate_session_file.py
@@ -1,3 +1,4 @@
+    
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
 # Licensed under the Raphielscape Public License, Version 1.b (the "License");
@@ -11,8 +12,8 @@ import os
 
 load_dotenv("config.env")
 
-API_KEY = os.environ.get("API_KEY", "930707")
-API_HASH = os.environ.get("API_HASH", "61db5228770fa752f871b4db48a9393f")
+API_KEY = os.environ.get("API_KEY", None)
+API_HASH = os.environ.get("API_HASH", None)
 
 bot = TelegramClient('userbot', API_KEY, API_HASH)
 bot.start()
